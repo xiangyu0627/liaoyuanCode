70700a0403dec4262a1e1fba8b059dc369ce568e
XiangyuZhong <xiangyu0627@gmail.com>
2016-07-19 17:50:28 -0600

    http replace

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -10,6 +10,8 @@ liaoyuanApp.controller('MainCtrl', ['$http', '$window', function ($http, $window
     scope.urlForm = '';
     // send POST request with original URL to server and geta short URL back.
     scope.getShortURL = function () {
+        scope.originalURL = scope.originalURL.replace('http://', '');
+        scope.originalURL = scope.originalURL.replace('https://', '');
         $http( { 
             method: 'POST', 
             url: '/getShortURL',
@@ -18,6 +20,7 @@ liaoyuanApp.controller('MainCtrl', ['$http', '$window', function ($http, $window
         .success(function (data, status) {
             // set the current short URL to data that got back from server  
             scope.shortURL = data.shortURL;
+            scope.oldOriginalURL = data.originalURL;
         })
         .error(function (data, status) {
             alert('Server error, please try again.');
@@ -32,6 +35,7 @@ liaoyuanApp.controller('MainCtrl', ['$http', '$window', function ($http, $window
         return scope.originalURL.length >= 5 || scope.urlForm.originalURL.$pristine;
     };
 
+    // send POST request with short URL  to server and get a original URL back.
     scope.redirect = function () {
         $http( { 
             method: 'POST', 
@@ -39,7 +43,7 @@ liaoyuanApp.controller('MainCtrl', ['$http', '$window', function ($http, $window
             data: { shortURL: scope.shortURL }
         })
         .success(function (data, status) {
-            // set the current short URL to data that got back from server  
+            // open link in new window 
             $window.open('http://' + data.originalURL);
         })
         .error(function (data, status) {
