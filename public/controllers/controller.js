977ee79917f5781cf0c1f8daa57ba28b322d30c0
XiangyuZhong <xiangyu0627@gmail.com>
2016-07-18 18:48:21 -0600

    Set up MongoDB and server functions

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,12 +4,27 @@ var liaoyuanApp = angular.module('liaoyuanApp', []);
 
 liaoyuanApp.controller('MainCtrl', ['$http', function ($http) {
     
-    this.originalURL = '';
-    this.shortURL = '';
+    var scope = this;
+    scope.originalURL = '';
+    scope.shortURL = '';
 
-    this.getShortURL = function () {
-        this.shortURL = this.originalURL + 'fdss';
-        this.originalURL = '';
+    // send POST request with original URL to server and geta short URL back.
+    scope.getShortURL = function () {
+        $http( { 
+            method: 'POST', 
+            url: '/getShortURL',
+            data: { originalURL: scope.originalURL }
+        })
+        .success(function (data, status) {
+            // set the current short URL to data that got back from server  
+            scope.shortURL = data.shortURL;
+        })
+        .error(function (data, status) {
+            alert('Server error, please try again.');
+        });
+        
+        // clear original URL field
+        scope.originalURL = '';
     };
 
 }]);
--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 var exoress = require('express');
 var app = exoress();
+var mongojs = require('mongojs');
+var db = mongojs('urlList', ['urlList']);
+var bodyParser = require('body-parser');
 
 app.use(exoress.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+/**
+ * Takes an URL and generates a shortened version of it
+ */
+function generateShortURL(originalURL) {
+    return '1234';
+}
+
+/**
+ * Listens to querys from front end, take input url and call generateShortURL,
+ * then save the result to database, and finally return the result to front end.
+ */
+app.post('/getShortURL', function (req, res) { 
+    var originalURL = req.body.originalURL;
+    var shortURL = generateShortURL(originalURL);
+
+    var result = {
+        originalURL: originalURL,
+        shortURL: shortURL
+    }
+
+    // insert to database and then responde to front end
+    db.urlList.insert(result, function (err, doc) {
+        res.json(doc);
+    });
+});
 
 app.listen(3000);
 console.log('Server running on port 3000');
